8e89cb952fc26f75bd24657527a1ced594d93f6e
Samantha Malone <sammac99@gmail.com>
2021-05-31 18:58:56 -0700

    updated js

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,4 +1,8 @@
 // function call
 var getUserRepos = function() {
-  fetch("https://api.github.com/users/octocat/repos");
+  fetch("https://api.github.com/users/octocat/repos").then(function(response) {
+  response.json().then(function(data) {
+    console.log(data);
+  });
+});
 };
